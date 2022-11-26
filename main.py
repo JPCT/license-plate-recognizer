a7d6d1a70bc0b35cd36b15dae7df87fcef08ec1c
JPCT <juan.pablo.999@outlook.com>
2022-11-26 18:02:20 -0500

    Attemps

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ from picamera import PiCamera
 import time
 import cv2
 import imutils
+import re
 from LPR import LPR
 
 camera = PiCamera()
+attemps = 1
+max_attemps = 20
 
 def detect_movement():
 	pir = MotionSensor(4)
@@ -17,17 +20,31 @@ def detect_movement():
 		print("Motion stopped")
 
 def take_picture():
-	time.sleep(2)
 	path = "imgs/img.jpg"
 	camera.capture(path)
-	print("Picture taken.")
+	print("Picture taken. Attemp #" + str(attemps))
 	recognize(path)
+
+def validate_characters(plate):
+	global attemps
+	pat = re.compile(r"([A-Z])([A-Z])([A-Z])([0-9])([0-9])([0-9])")
+	plate = plate.strip()
+	
+	if re.fullmatch(pat, plate):
+		attemps = 1
+		print(plate)
+	elif attemps < max_attemps:
+		attemps += 1
+		take_picture()
+	attemps = 1
+	
 
 def recognize(path):
 	lpr = LPR()
 	image = cv2.imread(path)
 	image = imutils.resize(image, height=500, width=500 )
-	print(lpr.read_license(image))
+	plate = lpr.read_license(image)
+	validate_characters(plate)
 
 def begin():
 	detect_movement()
